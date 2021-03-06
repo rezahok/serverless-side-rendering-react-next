00048e0c7011e35876c6a9dcdaedfe772f2857f3
Rezaul Hoque <rezahok@gmail.com>
2021-03-06 09:46:14 -0500

    Revert "Remove redundant files."
    This reverts commit b101f2f77bcd7adf64d28a441f4ed75d0c7cd013.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Default from '../layouts/default'
 import axios from 'axios'
 
-class NewsDetailsPage extends React.Component {
+class IndexPage extends React.Component {
   static async getInitialProps({ query: { newsId } }) {
 
     const { data } = await axios.get(
@@ -27,15 +27,13 @@ class NewsDetailsPage extends React.Component {
   async componentDidMount () {
     await this.fetchData()
   }
-
   async fetchData () {
     this.setState({ loading: true })
     const { data } = await axios.get(
-      'https://api.shapla.app/singlenews?id=' + newsId
+      'https://api.thedogapi.com/v1/images/search?limit=1'
     )
-
     this.setState({
-      newsItem: (data['news'])[0],
+      dog: data[0],
       loading: false
     })
   }
@@ -49,11 +47,13 @@ class NewsDetailsPage extends React.Component {
     return (
       <Default meta={meta}>
         <div>
-          <img src={this.state.newsItem.pic} alt='' />
+          <h1>This is the Front Page.</h1>
+          <h3>Random dog of the day:</h3>
+          <img src={this.state.dog.url} alt='' />
         </div>
       </Default>
     )
   }
 }
 
-export default NewsDetailsPage
+export default IndexPage
